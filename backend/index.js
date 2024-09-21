ecb823694a7b81fa688cc3eae9bbb9f3bfc2450a
Anurag Samdariya <anurag4851@gmail.com>
2024-09-21 22:41:53 +0530

    added more apis

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,8 @@ app.get("/",(req,res)=>{
     res.json({data:"hello"});
 });
 
+
+
 // Create Account API
 app.post("/create-account", async(req,res)=>{
     const { fullName, email, password } = req.body;
@@ -126,6 +128,28 @@ app.post("/login", async (req, res)=>{
     }
 });
 
+//Get user
+app.get("/get-user",authenticateToken, async(req, res)=>{
+    const {user} = req.user;
+
+    const isUser = await User.findOne({_id:user._id});
+
+    if(!isUser) {
+        return res.sendStatus(401);
+    }
+
+    return res.json({
+        user:{
+            fullName: isUser.fullName,
+            email: isUser.email,
+            _id: isUser._id,
+            createdOn: isUser.createdOn,
+        },
+        message:"",
+    });
+});
+
+// Add note
 app.post("/add-note", authenticateToken, async(req, res)=>{
     const { title, content, tags } = req.body;
     const { user } = req.user;
@@ -161,6 +185,124 @@ app.post("/add-note", authenticateToken, async(req, res)=>{
         });
     }
 });
+
+// Edit note
+app.put("/edit-note/:noteId", authenticateToken, async(req, res)=>{
+    const noteId = req.params.noteId;
+    const { title, content, tags, isPinned } = req.body;
+    const { user } = req.user;
+
+    if(!title && !content && !tags) {
+        return res
+            .status(400)
+            .json({error: true, message:"No changes provided"});
+    }
+
+    try {
+        const note = await Note.findOne({_id: noteId, userId: user._id});
+
+        if(!note) {
+            return res.status(404).json({error:true, message:"Note not found"});
+        }
+
+        if(title) note.title = title;
+        if(content) note.content = content;
+        if(tags) note.tags = tags;
+        if(isPinned) note.isPinned = isPinned;
+
+        await note.save();
+
+        return res.json({
+            error: false,
+            note,
+            message:"Note updated successfully",
+        });
+        
+    } catch (error) {
+        return res.status(500).json({
+            error:true,
+            message:"Internal server error",
+        });
+    }
+})
+
+//Get all notes
+app.get("/get-all-notes/", authenticateToken, async(req, res)=>{
+    const {user} = req.user;
+    
+    try {
+        const notes = await Note.find({userId: user._id}).sort({ isPinned : -1});
+        
+        return res.json({
+            error: false,
+            notes,
+            message:"All notes retrieved successfully",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            error:true,
+            message:"Internal server error",
+        });
+    }
+})
+
+// Delete note
+app.delete("/delete-note/:noteId", authenticateToken, async(req, res)=>{
+    const noteId = req.params.noteId;
+    const {user} = req.user;
+
+    try {
+        const note = await Note.findOne({_id:noteId, userId:user._id});
+
+        if(!note) {
+            return res.status(404).json({erro:true, message: "Note not found"
+            });
+        }
+
+        await Note.deleteOne({_id:noteId, userId:user._id});
+
+        return res.json({
+            error:false,
+            message:"Node deleted successfully",
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error:true,
+            message:"Internal server error"
+        })
+    }
+})
+
+//Update isPinned
+app.put("/update-note-pinned/:noteId", authenticateToken, async(req, res)=>{
+    const noteId = req.params.noteId;
+    const { isPinned } = req.body;
+    const { user } = req.user;
+
+    try {
+        const note = await Note.findOne({_id: noteId, userId: user._id});
+
+        if(!note) {
+            return res.status(404).json({error:true, message:"Note not found"});
+        }
+        
+        note.isPinned = isPinned;
+
+        await note.save();
+
+        return res.json({
+            error: false,
+            note,
+            message:"Note updated successfully",
+        });
+        
+    } catch (error) {
+        return res.status(500).json({
+            error:true,
+            message:"Internal server error",
+        });
+    }
+})
 
 
 
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,33 +1,140 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import NoteCard from "../../components/Cards/NoteCard";
+import EmptyCard from "../../components/EmptyCard/EmptyCard";
 import { MdAdd } from "react-icons/md";
 import AddEditNotes from "./AddEditNotes";
 import Modal from "react-modal";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import moment from "moment";
+import Toast from "../../components/ToastMessage/Toast";
+import addNotesImage from "../../assets/images/add-notes.svg";
 
 const Home = () => {
+  console.log("Here we are!");
+
   const [openAddEditModal, setOpenAddEditModal] = useState({
     isShown: false,
     type: "add",
     data: null,
   });
 
+  const [showToastMsg, setShowToastMsg] = useState({
+    isShown: false,
+    message: "",
+    type: "add",
+  });
+  const [allNotes, setAllNotes] = useState([]);
+  const [userInfo, setUserInfo] = useState(null);
+  const navigate = useNavigate();
+
+  const handleEdit = (noteDetails) => {
+    setOpenAddEditModal({ isShown: true, type: "edit", data: noteDetails });
+  };
+
+  const showToastMessage = (message, type) => {
+    setShowToastMsg({
+      isShown: true,
+      message,
+      type,
+    });
+  };
+
+  const handleCloseToast = () => {
+    setShowToastMsg({
+      isShown: false,
+      message: "",
+    });
+  };
+
+  // Get user info
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get("/get-user");
+      if (response.data && response.data.user) {
+        setUserInfo(response.data.user);
+      }
+    } catch (error) {
+      if (error.response.status === 401) {
+        localStorage.clear();
+        navigate("/login");
+      }
+    }
+  };
+
+  //Get all notes
+  const getAllNotes = async () => {
+    try {
+      const response = await axiosInstance.get("/get-all-notes");
+      console.log(response);
+      if (response.data && response.data.notes) {
+        setAllNotes(response.data.notes);
+      }
+    } catch (error) {
+      console.log("An unexppected error occured. Please try again.");
+    }
+  };
+
+  //Delete Note
+  const deleteNote = async (data) => {
+    const noteId = data._id;
+    try {
+      const response = await axiosInstance.delete("/delete-note/" + noteId);
+
+      if (response.data && !response.data.error) {
+        showToastMessage("Note deleted successfully", "delete");
+        getAllNotes();
+      }
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        console.log("An unexppected error occured. Please try again.");
+      }
+    }
+  };
+
+  useEffect(() => {
+    getAllNotes();
+    getUserInfo();
+    return () => {};
+  }, []);
+
   return (
     <>
-      <Navbar />
+      <Navbar userInfo={userInfo} />
       <div className="container mx-auto">
-        <div className="grid grid-cols-3 gap-4 mt-8">
-          <NoteCard
-            title="Meeting on 7th April"
-            date="3rd Apr 2024"
-            content="Meeting on 7th AprilMeeting on 7th April"
-            tags="Meeting"
-            isPinned={true}
-            onEdit={() => {}}
-            onDelete={() => {}}
-            onPinNote={() => {}}
+        {allNotes.length > 0 ? (
+          <div className="grid grid-cols-3 gap-4 mt-8">
+            {allNotes.map((item, index) => {
+              console.log(item);
+              return (
+                <NoteCard
+                  key={item._id}
+                  // key={index}
+                  title={item.title}
+                  date={item.createdOn}
+                  content={item.content}
+                  tags={item.tags}
+                  isPinned={item.isPinned}
+                  onEdit={() => handleEdit(item)}
+                  // onEdit={() => setOpenAddEditModal({isShown:true, data: item, type:"edit"})}
+                  onDelete={() => deleteNote(item)}
+                  onPinNote={() => {}}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <EmptyCard
+            imgSrc={addNotesImage}
+            message={`Start creating your first note! Click the 'Add' button to jot down your thoughts , 
+        ideas and reminders. Lets get started!`}
           />
-        </div>
+        )}
       </div>
 
       <button
@@ -50,14 +157,22 @@ const Home = () => {
         contentLabel="Add/Edit Notes Modal"
         className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 "
       >
-        <AddEditNotes 
+        <AddEditNotes
           type={openAddEditModal.type}
-          nodeData={openAddEditModal.data}
-          onClose={()=>{
-            setOpenAddEditModal({ isShown:false, type:"add", data:null });
+          noteData={openAddEditModal.data}
+          onClose={() => {
+            setOpenAddEditModal({ isShown: false, type: "add", data: null });
           }}
+          getAllNotes={getAllNotes}
+          showToastMessage={showToastMessage}
         />
       </Modal>
+      <Toast
+        isShown={showToastMsg.isShown}
+        message={showToastMsg.message}
+        type={showToastMsg.type}
+        onClose={handleCloseToast}
+      />
     </>
   );
 };
